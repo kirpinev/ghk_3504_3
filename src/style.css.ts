ed41306981f1bad4f4252f96ccd986139ad5c98e
Igor Kirpinev <kirpinevigor@yandex.ru>
2024-11-03 14:01:14 +0300

    init

---
+++ src/style.css.ts
@@ -0,0 +1,90 @@
+import { style } from "@vanilla-extract/css";
+
+const container = style({
+  display: "flex",
+  padding: "1rem",
+  flexDirection: "column",
+  // gap: "1rem",
+});
+
+const box = style({
+  textAlign: "center",
+});
+
+const bottomBtn = style({
+  position: "fixed",
+  zIndex: 2,
+  width: "100%",
+  padding: "12px",
+  bottom: 0,
+});
+
+const productsTitle = style({
+  marginBottom: "0.5rem",
+});
+
+const products = style({
+  display: "flex",
+  gap: "1rem",
+});
+
+const product = style({
+  backgroundColor: "#F2F3F5",
+  borderRadius: "1rem",
+  padding: "1rem",
+  display: "flex",
+  justifyContent: "space-between",
+  gap: "0.2rem",
+});
+
+const productTitle = style({
+  lineHeight: "24px",
+  fontSize: "17px",
+  marginBottom: "0.3rem",
+});
+
+const bonus = style({
+  flex: 1,
+});
+
+const gifts = style({
+  display: "flex",
+  flexWrap: "wrap",
+  gap: "0.5rem",
+});
+
+const gift = style({
+  border: "2px solid gray",
+  borderRadius: "1rem",
+  borderColor: "#F2F3F5",
+  padding: "0.5rem",
+  boxSizing: "border-box",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+  flex: 1,
+  flexBasis: "30%",
+  height: "4rem",
+  cursor: "pointer",
+  position: "relative",
+});
+
+const checkMark = style({
+  position: "absolute",
+  right: -3,
+  top: -9,
+});
+
+export const appSt = {
+  bottomBtn,
+  container,
+  box,
+  productsTitle,
+  products,
+  productTitle,
+  product,
+  bonus,
+  gifts,
+  gift,
+  checkMark,
+};
